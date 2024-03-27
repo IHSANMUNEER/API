adf347268f88928f7f3ff94bfee46266fa0c4586
Ihsan Muneer <72526107+IHSANMUNEER@users.noreply.github.com>
2024-03-27 11:28:37 +0500

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ var app=express();
 require("dotenv").config()
 const db  = require("./Db/Connection")
 
-const port = 3000
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 const product_routes = require("./Routes/Route");
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.use("/products",product_routes)
 
 
-app.listen(port, async()=>{
+app.listen(port, "0.0.0.0", function () {
     await db(process.env.MONGODB_URL);
     //await model.create(productJson)
     console.log('listening on port 3000')
